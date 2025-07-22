a2e2517553834d7980ac7984a73b323420cd7652
andrewrusso <and.russo.0511@gmail.com>
2025-07-23 01:50:16 +0200

    # Commit 2 - added pygame - the game is now only a black background with a green dot that can move to the left or to the right - in the future shooting capabilities will be added + enemies to shoot and point system.

--- main.py
+++ main.py
@@ -1,3 +1,39 @@
 #Cosmic Raiders Version 0.1
+import pygame
+# import random to generate enemy positions?
 
-print("Welcome to Cosmic Raiders")
+#Initialization
+pygame.init() #Initialize pygame modules
+screen = pygame.display.set_mode((800, 600)) #setup the game window
+pygame.display.set_caption("Cosmic Raiders")
+clock = pygame.time.Clock() #to check game speed
+
+# Player
+player_img = pygame.Surface((50, 30))
+player_img.fill((0, 255, 0))
+player_x = 375
+player_y = 540
+player_speed = 5
+
+# Main Game Loop
+running = True
+while running:
+    clock.tick(60) #to cap speed at 60fps 
+    screen.fill((0, 0, 0))  # black background
+    # Quit the game
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Player Controls
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and player_x > 0:
+        player_x -= player_speed
+    if keys[pygame.K_RIGHT] and player_x < 800 - 50:
+        player_x += player_speed
+
+    # Player Img
+    screen.blit(player_img, (player_x, player_y))
+    pygame.display.flip()  # aggiorna il display
+
+pygame.quit()
