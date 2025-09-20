27f6f47719530e659e7c92f43b5019899d78fd81
Genny4Real1 <and.russo.0511@gmail.com>
2025-09-20 06:38:52 +0200

    added enemies larger screen expanded player movement added enemy spawning and movement
    TODO:
    enemy behaviour
    pew pew mecchanic
    enemy respawning
    score tracking

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 #Cosmic Raiders Version 0.1
 import pygame
-# import random to generate enemy positions?
+import random
 
 #Initialization
 pygame.init() #Initialize pygame modules
-screen = pygame.display.set_mode((800, 600)) #setup the game window
+screen = pygame.display.set_mode((1600, 900)) #setup the game window
 pygame.display.set_caption("Cosmic Raiders")
 clock = pygame.time.Clock() #to check game speed
+dt = pygame.time.get_ticks()/ 1000
 
 # Player
-player_img = pygame.Surface((50, 30))
+player_img = pygame.Surface((60, 40))
 player_img.fill((0, 255, 0))
-player_x = 375
-player_y = 540
-player_speed = 5
+player = {'x':  800, 'y': 800, 'speed': 7.5}
+
+# Enemies
+enemy_img = pygame.Surface((30, 30))
+enemy_img.fill((252, 0, 0))
+enemies = []
+for i in range(5):
+    while True:
+        x = random.randint(20,1480)
+        if all(x != e['x'] for e in enemies):
+            break
+    enemies.append({'x': x, 'y': (random.randint(20, 90)), 'speed': 0.05})
 
 # Main Game Loop
 running = True
@@ -27,13 +37,36 @@ while running:
 
     # Player Controls
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and player_x > 0:
-        player_x -= player_speed
-    if keys[pygame.K_RIGHT] and player_x < 800 - 50:
-        player_x += player_speed
+    if (keys[pygame.K_LEFT] or keys[pygame.K_a]) and player['x'] > 0:
+        player['x'] -= player['speed']
+        print(f"x: {player['x']}, y: {player['y']}")
+    if (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and player['x'] < 1600 - 60:
+        player['x'] += player['speed']
+        print(f"x: {player['x']}, y: {player['y']}")
+    if (keys[pygame.K_DOWN] or keys[pygame.K_s]) and player['y'] < 900 - 40:
+        player['y'] += player['speed']
+        print(f"x: {player['x']}, y: {player['y']}")
+    if (keys[pygame.K_UP] or keys[pygame.K_w]) and player['y'] > 0:
+        player['y'] -= player['speed']
+        print(f"x: {player['x']}, y: {player['y']}")
+    if keys[pygame.K_ESCAPE]:
+        running = False
+
+    # Enemies behaviour
+
+
+    for enemy in enemies:
+        screen.blit(enemy_img, (enemy['x'], enemy['y']))
+        enemy['y'] += enemy['speed']
+
+        enemy['speed'] += 0.01 * dt
+
+
+
+
 
     # Player Img
-    screen.blit(player_img, (player_x, player_y))
+    screen.blit(player_img, (player['x'], player['y']))
     pygame.display.flip()  # aggiorna il display
 
 pygame.quit()
